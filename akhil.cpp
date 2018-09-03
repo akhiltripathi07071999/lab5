14620adfdf7a174d5b4dffda73aba00d3a396b6f
akhiltripathi07071999 <42205097+akhiltripathi07071999@users.noreply.github.com>
2018-09-03 17:26:15 +0530

    Add files via upload

---
+++ akhil.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1,num2;
+cout<<"enter the first number"<<endl;
+cin>>num1;
+cout<<"enter the second number"<<endl;
+cin>>num2;
+if (num1>num2)
+cout<<num1<<"is maximum"<<endl;
+else 
+cout<<num2<<"is maximum"<<endl;
+return 0;
+}
