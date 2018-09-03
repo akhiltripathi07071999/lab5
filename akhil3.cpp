d5f9d8721cea940addae4959a6a62025528a8178
akhiltripathi07071999 <42205097+akhiltripathi07071999@users.noreply.github.com>
2018-09-03 18:38:16 +0530

    Add files via upload

---
+++ akhil3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int number;
+cout<<"enter a number"<<endl;
+cin>>number;
+if (number > 0)
+cout<<"number is positive"<<endl;
+else if (number < 0)
+cout<<"number is negative"<<endl;
+else                                                                   
+cout<<"number is zero"<<endl;
+return 0;
+}
