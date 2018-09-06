c62a1989c891e5f379fcdd6c710b57b4c85ff72d
akhiltripathi07071999 <42205097+akhiltripathi07071999@users.noreply.github.com>
2018-09-06 11:50:57 +0530

    Add files via upload

---
+++ akhil8.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<< "print numbers in reverse direction"<<endl;
+cout<< "enter the numbers"<<endl;
+cin>>n;
+int i=n;
+while (i>=1)
+cout<<i--<<endl;
+return 0;
+}
